d64ebd5fcdf784ebdd8a622f17322b0927ba8206
dylanh2o <dylan.carlu@gmail.com>
2020-12-16 01:26:57 +0100

    2. Transfer Tokens

--- contracts/SAEToken.sol
+++ contracts/SAEToken.sol
@@ -14,8 +14,14 @@ contract SAEToken {
         uint256 _value
     );
 
-    mapping(address => uint256) public balanceOf;
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
 
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
     // Constructor
     constructor (uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
@@ -31,6 +37,29 @@ contract SAEToken {
         //Transfer Event
         emit Transfer(msg.sender, _to, _value);
         //return a boolean
+        return true;
+    }
+
+    // approve
+    function approve(address _spender, uint256 _value) public returns (bool success){
+        // allowance
+        allowance[msg.sender][_spender] = _value;
+        // Approve event
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    // transferFrom
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
         return true;
     }
 }
