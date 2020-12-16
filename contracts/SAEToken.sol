2e38fcb01ccf53ff29c82d228c245ef0d26429de
dylanh2o <dylan.carlu@gmail.com>
2020-12-16 01:36:28 +0100

    3.

--- contracts/SAEToken.sol
+++ contracts/SAEToken.sol
@@ -1,36 +1,13 @@
 pragma solidity ^0.5.16;
 
 contract SAEToken {
-    //Name
-    string public name = "SAE Token";
-    //Symbol
-    string public symbol = "SAE";
-    string public standard = "SAE Token v1.0";
+
+    // Set the total number of tokens
+    // Read the total number of tokens
     uint256 public totalSupply;
-
-    event Transfer(
-        address indexed _from,
-        address indexed _to,
-        uint256 _value
-    );
-
-    mapping(address => uint256) public balanceOf;
-
     // Constructor
-    constructor (uint256 _initialSupply) public {
-        balanceOf[msg.sender] = _initialSupply;
-        totalSupply = _initialSupply;
-    }
-    //transfer
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        //Exception if account does't have enough
-        require(balanceOf[msg.sender] >= _value);
-        //Transfer the balance
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value;
-        //Transfer Event
-        emit Transfer(msg.sender, _to, _value);
-        //return a boolean
-        return true;
+    constructor () public {
+        totalSupply = 1000000;
+
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const SAEToken = artifacts.require("SAEToken");
 
 module.exports = function (deployer) {
-	deployer.deploy(SAEToken, 1000000);
+	deployer.deploy(SAEToken);
 };
